3d40c4dcbd58ddc786ba6834329692c8e6f2f9ae
KRISHNA KUMAR MANDAL <kkmandal2030@gmail.com>
2025-01-04 17:11:11 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import Navbar from "./Sections/Navbar";
+
+export default function App() {
+  return <Navbar />;
+}
---
+++ src/Sections/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#0B1120] ">
+      <div class="bg-blue-500 p-4 sm:bg-green-500 md:bg-red-500 lg:bg-yellow-500 xl:bg-purple-500">
+        Responsive Box
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
