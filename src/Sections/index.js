cc2e48e5bfdd9471ff0a16a78abd301e25761b71
KRISHNA KUMAR MANDAL <kkmandal2030@gmail.com>
2025-01-05 17:23:12 +0530

    add hero section and design
    mejor change

---
+++ src/Sections/index.js
@@ -0,0 +1,39 @@
+import ff1 from "../assets/freefire1.jpg";
+import ff2 from "../assets/freefire2.jpg";
+import ff3 from "../assets/freefire3.jpg";
+import ff4 from "../assets/freefire4.jpg";
+import ff5 from "../assets/freefire5.jpg";
+import ff6 from "../assets/freefire6.jpg";
+
+export const data = [
+  {
+    ChannelName: "Puspa Gamer",
+    img: { ff1 },
+    earning: "~17k/month",
+  },
+  {
+    ChannelName: "M1MX B4K",
+    img: { ff2 },
+    earning: "~60k/month",
+  },
+  {
+    ChannelName: "OMG FREE FIRE",
+    img: { ff3 },
+    earning: "~15k/month",
+  },
+  {
+    ChannelName: "ZX Azad",
+    img: { ff4 },
+    earning: "~19k/month",
+  },
+  {
+    ChannelName: "Sohan FF",
+    img: { ff5 },
+    earning: "~21k/month",
+  },
+  {
+    CahnnelName: "Shadow Apartan Gaming",
+    img: { ff6 },
+    earning: "~Not y",
+  },
+];
